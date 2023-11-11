e43c5a6f9bceef4a731abf0676a818335d503735
Gerrard Sebastian <114888829+gerrardgs@users.noreply.github.com>
2023-11-11 12:57:31 +0700

    Update sourceCodes26.c

--- cCollege/sourceCodes26.c
+++ cCollege/sourceCodes26.c
@@ -1,1 +1,14 @@
 
+#include <stdio.h>
+
+int main() {
+
+  FILE *fptr;
+  fptr = fopen("saveone.txt","a");
+  char kalimat[1000];
+  printf ("Masukkan kalimat Tambahan : ");
+  gets (kalimat);
+  fprintf(fptr,"\n%s", kalimat);
+  fclose(fptr);
+
+}
