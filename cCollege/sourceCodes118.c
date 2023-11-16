82ae0a0e8ffd5a71c668f6ecbf081a33bb6de706
Gerrard Sebastian <114888829+gerrardgs@users.noreply.github.com>
2023-11-16 22:51:03 +0700

    Create sourceCodes118.c

---
+++ cCollege/sourceCodes118.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main (){
+    int penjumlahan(int a, int b){
+        int c = a + b;
+        return c;
+    }
+
+    void tampil(int a, int b){
+        int c = a + b;
+        printf("%d", c);    
+    }
+}
