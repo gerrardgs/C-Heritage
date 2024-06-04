6ebfcbbc626d8f7e06a7749f1e1a4cb763706787
Gerrard Sebastian <114888829+gerrardgs@users.noreply.github.com>
2024-06-04 12:42:10 +0700

    Update TUBES.cpp
    UPDATE & DEBUGGING

--- cCollege/InternetOfThings/TUBESUAS/TUBES.cpp
+++ cCollege/InternetOfThings/TUBESUAS/TUBES.cpp
@@ -1,106 +1,104 @@
 #include <Stepper.h>
+#include <WiFi.h>
+#define BLYNK_TEMPLATE_ID "TMPL6NrA97ZDC"   // Define Template ID here
+#define BLYNK_TEMPLATE_NAME "JEMURAN IoT"  // Define Template Name here
+#include <BlynkSimpleEsp32.h>
 
-const int rainSensorPin = 25;
-const int ldrSensorPin = 2;
-const int buzzerPin = 5;
-const int stepsPerRevolution = 2048;
-
+// Define pins and constants
 #define IN1 19
 #define IN2 18
 #define IN3 5
 #define IN4 17
+#define RAIN_SENSOR_PIN 25
+#define LDR_SENSOR_PIN 2
+
+#define BLYNK_AUTH_TOKEN "4rDUyYxIfU57gEnuos1to-3o0Sy6FsNF"
+
+const int stepsPerRevolution = 4096;
+const int positionOut = 5000;
+const int positionIn = 0;
 
 Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
 
+int currentPosition = 0;
+bool isManualControl = false;
+
+char auth[] = "4rDUyYxIfU57gEnuos1to-3o0Sy6FsNF";
+char ssid[] = "P4INCE";
+char pass[] = "1stCentralBorneo";
+
+BlynkTimer timer;
+
 void setup() {
-  pinMode(rainSensorPin, INPUT);  // Set sensorPin as input
-  analogReadResolution(10);
-  pinMode(ldrSensorPin, OUTPUT);
-  pinMode(buzzerPin, OUTPUT);
-  Serial.begin(9600);         // Start serial communication at 9600 baud rate
-  myStepper.setSpeed(5);
   Serial.begin(9600);
+  myStepper.setSpeed(5);
+  Blynk.begin(auth, ssid, pass);
+  timer.setInterval(1000L, sensorRead);
+}
+
+BLYNK_WRITE(V1) {
+  int buttonState = param.asInt();
+  isManualControl = true;
+  if (buttonState == 1) { 
+    runToNewPosition(positionOut);
+    Blynk.logEvent("jemuran_keluar", "Jemuran Bergerak Keluar");
+  } else {
+    runToNewPosition(positionIn);
+    Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk Kedalam");
+  }
 }
 
 void loop() {
-  rainSensor();
-  delay(500);
-  ldrSensor();
-  delay(500);
-  // stepper();
-  // delay(1000);
-
-if (ldrSensor() <= 300 && (rainSensor() >= 700 && rainSensor() <= 1000)) {
-    myStepper.step(stepsPerRevolution);
+  Blynk.run();
+  timer.run();
 }
 
+void sensorRead() {
+  if (!isManualControl) {
+    int rainValue = analogRead(RAIN_SENSOR_PIN);
+    Serial.print("Raindrop Sensor Value: ");
+    Serial.println(rainValue);
+
+    int ldrValue = analogRead(LDR_SENSOR_PIN);
+    Serial.print("LDR Sensor Value: ");
+    Serial.println(ldrValue);
+
+    bool isRaining = rainValue < 700;
+    bool isBright = ldrValue > 570;
+    bool isDark = ldrValue < 300;
+
+    Blynk.virtualWrite(V2, isRaining ? "Hujan" : "Tidak Hujan");
+    Blynk.virtualWrite(V3, isBright ? "Cerah/Terang" : (isDark ? "Gelap" : "Normal"));
+
+    if (!isRaining && isBright) {
+      Serial.println("Tidak hujan dan terang - Jemuran keluar");
+      runToNewPosition(positionOut);
+      Blynk.logEvent("jemuran_keluar", "Jemuran Bergerak Keluar, Status: Terang/Cerah, Tidak Hujan");
+    } else if (!isRaining && isDark) {
+      Serial.println("Tidak hujan dan gelap - Jemuran masuk");
+      runToNewPosition(positionIn);
+      Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk, Status: Gelap, Tidak Hujan");
+    } else if (isRaining && isBright) {
+      Serial.println("Hujan dan terang - Jemuran masuk");
+      runToNewPosition(positionIn);
+      Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk, Status: Terang/Cerah, Hujan");
+    } else if (isRaining && isDark) {
+      Serial.println("Hujan dan gelap - Jemuran masuk");
+      runToNewPosition(positionIn);
+      Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk, Status: Gelap, Hujan");
+    }
+  }
+
+  if (isManualControl) {
+    delay(2000);
+    isManualControl = false;
+  }
 }
 
-int rainSensor() {
-  // Serial.println("Raindrop : ");
-  int nilai = analogRead(rainSensorPin);
-  Serial.print("\nRaindrop: " + String(nilai));  // Read the digital value from the sensor and print it to the serial monitor
-  // delay(50);
-  // if (digitalRead(rainSensorPin)==0) {
-  //   tone(buzzerPin, 250);
-  // } else if (digitalRead(rainSensorPin)==1) {
-  //   tone(buzzerPin, 0);
-  // }
-  return nilai;
+void runToNewPosition(int newPosition) {
+  int steps = newPosition - currentPosition;
+  if (steps != 0) {
+    myStepper.step(steps);
+    currentPosition = newPosition;
+  }
 }
-
-int ldrSensor() {
-  // int lightState = analogRead(ldrSensorPin);
-  // if (lightState == HIGH) {
-  //   Serial.println("Dark");
-  // } else {
-  //   Serial.println("Light");
-  // }
-  int nilai = (analogRead(ldrSensorPin));
-  Serial.print("\nLDR: " + String(nilai));
-  return nilai;
-}
-
-void stepper(){
-  // step one revolution in one direction:
-  Serial.println("clockwise");
-  myStepper.step(stepsPerRevolution);
-  delay(1000);
-
-  // step one revolution in the other direction:
-  Serial.println("counterclockwise");
-  myStepper.step(-stepsPerRevolution);
-  delay(1000);
-}
-
-// #include <Stepper.h>
-
-// const int stepsPerRevolution = 2048;  // change this to fit the number of steps per revolution
-
-// // ULN2003 Motor Driver Pins
-// #define IN1 19
-// #define IN2 18
-// #define IN3 5
-// #define IN4 17
-
-// // initialize the stepper library
-// Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
-
-// void setup() {
-//   // set the speed at 5 rpm
-//   myStepper.setSpeed(5);
-//   // initialize the serial port
-//   Serial.begin(115200);
-// }
-
-// void loop() {
-//   // step one revolution in one direction:
-//   Serial.println("clockwise");
-//   myStepper.step(stepsPerRevolution);
-//   delay(1000);
-
-//   // step one revolution in the other direction:
-//   Serial.println("counterclockwise");
-//   myStepper.step(-stepsPerRevolution);
-//   delay(1000);
-// }
