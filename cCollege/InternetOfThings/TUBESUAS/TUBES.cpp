3206a871d99e59794866b12c73f7f3398dc2f7ab
Gerrard Sebastian <114888829+gerrardgs@users.noreply.github.com>
2024-06-19 22:04:17 +0700

    Update TUBES.cpp

--- cCollege/InternetOfThings/TUBESUAS/TUBES.cpp
+++ cCollege/InternetOfThings/TUBESUAS/TUBES.cpp
@@ -1,104 +1,85 @@
-#include <Stepper.h>
-#include <WiFi.h>
-#define BLYNK_TEMPLATE_ID "TMPL6NrA97ZDC"   // Define Template ID here
-#define BLYNK_TEMPLATE_NAME "JEMURAN IoT"  // Define Template Name here
-#include <BlynkSimpleEsp32.h>
+#define BLYNK_AUTH_TOKEN "8gTndKZHrpDzGZo3XSH3hi-sNTnqB5Aq"
+#define BLYNK_TEMPLATE_ID "TMPL6JIY2uARx"
+#define BLYNK_TEMPLATE_NAME "CONTROL AND MONITORING JEMURAN IoT"
 
-// Define pins and constants
+#include <WiFi.h>
+#include <BlynkSimpleEsp32.h>
+#include <Stepper.h>
+
 #define IN1 19
 #define IN2 18
 #define IN3 5
 #define IN4 17
-#define RAIN_SENSOR_PIN 25
-#define LDR_SENSOR_PIN 2
+#define RAIN_SENSOR_PIN 33
+#define LDR_SENSOR_PIN 32
+#define STEPS 100
 
-#define BLYNK_AUTH_TOKEN "4rDUyYxIfU57gEnuos1to-3o0Sy6FsNF"
+char auth[] = BLYNK_AUTH_TOKEN;
+char ssid[] = "Infomu tah Infoku";
+char pass[] = "ngopi1234";
 
-const int stepsPerRevolution = 4096;
-const int positionOut = 5000;
-const int positionIn = 0;
+Stepper myStepper(STEPS, IN1, IN3, IN2, IN4);
 
-Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+bool isClothesOutside = false;
 
-int currentPosition = 0;
-bool isManualControl = false;
-
-char auth[] = "4rDUyYxIfU57gEnuos1to-3o0Sy6FsNF";
-char ssid[] = "P4INCE";
-char pass[] = "1stCentralBorneo";
-
-BlynkTimer timer;
-
-void setup() {
-  Serial.begin(9600);
-  myStepper.setSpeed(5);
-  Blynk.begin(auth, ssid, pass);
-  timer.setInterval(1000L, sensorRead);
+void moveClothesOutside()
+{
+    Serial.println("Jemuran Keluar!");
+    myStepper.step(7500);
+    isClothesOutside = true;
 }
 
-BLYNK_WRITE(V1) {
-  int buttonState = param.asInt();
-  isManualControl = true;
-  if (buttonState == 1) { 
-    runToNewPosition(positionOut);
-    Blynk.logEvent("jemuran_keluar", "Jemuran Bergerak Keluar");
-  } else {
-    runToNewPosition(positionIn);
-    Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk Kedalam");
-  }
+void moveClothesInside()
+{
+    Serial.println("Jemuran Masuk!");
+    myStepper.step(-7500);
+    isClothesOutside = false;
 }
 
-void loop() {
-  Blynk.run();
-  timer.run();
+void setup()
+{
+    Serial.begin(9600);
+    Blynk.begin(auth, ssid, pass);
+    pinMode(RAIN_SENSOR_PIN, INPUT);
+    pinMode(LDR_SENSOR_PIN, INPUT);
+    myStepper.setSpeed(100);
 }
 
-void sensorRead() {
-  if (!isManualControl) {
-    int rainValue = analogRead(RAIN_SENSOR_PIN);
-    Serial.print("Raindrop Sensor Value: ");
-    Serial.println(rainValue);
+void loop()
+{
+    Blynk.run();
 
     int ldrValue = analogRead(LDR_SENSOR_PIN);
-    Serial.print("LDR Sensor Value: ");
+    ldrValue = constrain(ldrValue, 0, 1023);
+    int rainValue = analogRead(RAIN_SENSOR_PIN);
+    rainValue = constrain(rainValue, 0, 1023);
+
+    Serial.print("Rain Sensor: ");
+    Serial.println(rainValue);
+    Serial.print("LDR Sensor: ");
     Serial.println(ldrValue);
 
-    bool isRaining = rainValue < 700;
-    bool isBright = ldrValue > 570;
-    bool isDark = ldrValue < 300;
+    Blynk.virtualWrite(V1, rainValue);
+    Blynk.virtualWrite(V2, ldrValue);
 
-    Blynk.virtualWrite(V2, isRaining ? "Hujan" : "Tidak Hujan");
-    Blynk.virtualWrite(V3, isBright ? "Cerah/Terang" : (isDark ? "Gelap" : "Normal"));
+    bool isRain = rainValue < 700;
+    bool isLight = ldrValue > 750;
+    bool isDark = ldrValue < 750;
 
-    if (!isRaining && isBright) {
-      Serial.println("Tidak hujan dan terang - Jemuran keluar");
-      runToNewPosition(positionOut);
-      Blynk.logEvent("jemuran_keluar", "Jemuran Bergerak Keluar, Status: Terang/Cerah, Tidak Hujan");
-    } else if (!isRaining && isDark) {
-      Serial.println("Tidak hujan dan gelap - Jemuran masuk");
-      runToNewPosition(positionIn);
-      Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk, Status: Gelap, Tidak Hujan");
-    } else if (isRaining && isBright) {
-      Serial.println("Hujan dan terang - Jemuran masuk");
-      runToNewPosition(positionIn);
-      Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk, Status: Terang/Cerah, Hujan");
-    } else if (isRaining && isDark) {
-      Serial.println("Hujan dan gelap - Jemuran masuk");
-      runToNewPosition(positionIn);
-      Blynk.logEvent("jemuran_masuk", "Jemuran Bergerak Masuk, Status: Gelap, Hujan");
+    if (!isClothesOutside)
+    {
+        if (isLight && !isRain)
+        {
+            moveClothesOutside();
+        }
     }
-  }
+    else
+    {
+        if (isRain || isDark)
+        {
+            moveClothesInside();
+        }
+    }
 
-  if (isManualControl) {
-    delay(2000);
-    isManualControl = false;
-  }
-}
-
-void runToNewPosition(int newPosition) {
-  int steps = newPosition - currentPosition;
-  if (steps != 0) {
-    myStepper.step(steps);
-    currentPosition = newPosition;
-  }
+    delay(1000);
 }
